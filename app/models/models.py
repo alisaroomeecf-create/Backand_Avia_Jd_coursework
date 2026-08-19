06d419821a46568537c99827d59bc9587d570092
alisaroomeecf-create <alisaroomeecf@gmail.com>
2026-08-19 18:16:21 +0300

    Update models.py

--- app/models/models.py
+++ app/models/models.py
@@ -24,7 +24,7 @@ class User(Base):
     phone: Mapped[str] = mapped_column(String, nullable=True)
     role: Mapped[str] = mapped_column(String, default='user')
 
-    # Связь: один пользователь -> много бронирований
+   
     bookings: Mapped[list["Booking"]] = relationship(back_populates="user")
 
 
@@ -34,9 +34,7 @@ class Trip(Base):
     from_city: Mapped[str] = mapped_column(String)
     to_city: Mapped[str] = mapped_column(String)
     price: Mapped[float] = mapped_column(Float)
-    # Можно добавить дату, если нужно: departure_date: Mapped[datetime] = mapped_column(nullable=False)
-
-    # Связь: один рейс -> много бронирований
+ 
     bookings: Mapped[list["Booking"]] = relationship(back_populates="trip")
 
 
@@ -46,10 +44,10 @@ class Booking(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     trip_id: Mapped[int] = mapped_column(ForeignKey('trips.id'))
 
-    # Используем Enum для надежности, но храним как строку в БД
+ 
     status: Mapped[str] = mapped_column(SQLEnum(BookingStatus), default=BookingStatus.PENDING)
     is_paid: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    # Обратные связи
+ 
     user: Mapped["User"] = relationship(back_populates="bookings")
     trip: Mapped["Trip"] = relationship(back_populates="bookings")
